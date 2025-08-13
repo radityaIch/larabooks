e3aa675ba621c674d32ca10e4ecfba36bf0c365a
radityaIch <raditya2678@gmail.com>
2025-08-14 06:54:32 +0800

    chore: add route alias in root page

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 
-Route::get('/', [BookController::class, 'index']);
+Route::get('/', [BookController::class, 'index'])->name('home');
 Route::get('/famous-authors', [BookController::class, 'showFamousAuthor'])->name('famous_authors');
 Route::get('/input-rating', [BookController::class, 'showInputRating'])->name('input_rating');
 Route::post('/input-rating', [BookController::class, 'storeInputRating'])->name('input_rating.store');
