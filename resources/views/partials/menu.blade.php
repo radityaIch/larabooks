3fa7a7e838650a47d9a28104b027958bf1ecb2ef
radityaIch <raditya2678@gmail.com>
2025-08-13 21:41:53 +0800

    chore: turn static route name into dynamic

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -4,8 +4,8 @@
       <div class="text-white text-lg font-bold">{{ env('APP_NAME') }}</div>
       <ul class="flex space-x-4">
         <li><a href="/" class="text-gray-300 hover:text-white">Home</a></li>
-        <li><a href="/famous-authors" class="text-gray-300 hover:text-white">Famous Author</a></li>
-        <li><a href="/input-rating" class="text-gray-300 hover:text-white">Input Rating</a></li>
+        <li><a href={{ route('famous_authors') }} class="text-gray-300 hover:text-white">Famous Author</a></li>
+        <li><a href={{ route('input_rating') }} class="text-gray-300 hover:text-white">Input Rating</a></li>
       </ul>
     </div>
   </div>
