b001ce3266480f6f6c6607fd73abec2df9abdb9d
radityaIch <raditya2678@gmail.com>
2025-08-13 21:40:32 +0800

    refactor: declaring the route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [BookController::class, 'index']);
+Route::get('/famous-authors', [BookController::class, 'showFamousAuthor'])->name('famous_authors');
+Route::get('/input-rating', [BookController::class, 'showInputRating'])->name('input_rating');
+Route::post('/input-rating', [BookController::class, 'storeInputRating'])->name('input_rating.store');
